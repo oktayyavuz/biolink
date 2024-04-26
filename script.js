79cc27ce129a14c7d213166da84a10a3afe4a082
oktayyavuz <76921027+oktayyavuz@users.noreply.github.com>
2024-04-26 10:51:53 +0300

    Köklü Değişim Dyto BioLink

---
+++ script.js
@@ -0,0 +1,53 @@
+
+const actionLink = document.querySelectorAll(".link-card .link-action");
+
+actionLink.forEach((action) => {
+  action.addEventListener("click", (e) => {
+    e.preventDefault();
+    navigator.clipboard.writeText(action.parentElement.getAttribute("href"));
+
+    document.getElementById("toast").innerHTML = `
+        <div class="toast-container">
+            <p>✅ Link <strong> ${action.parentElement.innerText} </strong> berhasil disalin!</p>
+        </div>
+    `;
+
+
+    setTimeout(() => {
+      document
+        .querySelector("#toast .toast-container")
+        .classList.add("toast-gone");
+    }, 300);
+
+    setTimeout(() => {
+      document.querySelector("#toast .toast-container").remove();
+    }, 2000);
+  });
+});
+
+
+
+document.querySelectorAll(".sosmed i").forEach((sosmed) => {
+  sosmed.addEventListener("mouseenter", () => {
+    sosmed.classList.remove("ph");
+    sosmed.classList.add("ph-fill");
+  });
+
+  sosmed.addEventListener("mouseleave", () => {
+    sosmed.classList.remove("ph-fill");
+    sosmed.classList.add("ph");
+  });
+});
+
+
+document.addEventListener("scroll", (e) => {
+  document.querySelector(".bg-text-animation").style.transform = `translateX(${
+    window.scrollY / 5
+  }px)`;
+});
+
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", () => {
+  preloader.style.display = "none";
+});
