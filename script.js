038a59376f80e3773b1e58e401cf5562b2b5fdbf
oktayyavuz <76921027+oktayyavuz@users.noreply.github.com>
2024-04-29 18:56:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
+const actionLinks = document.querySelectorAll(".link-card .link-action");
 
-const actionLink = document.querySelectorAll(".link-card .link-action");
+actionLinks.forEach((action) => {
+    action.addEventListener("click", (e) => {
+        e.preventDefault();
+        navigator.clipboard.writeText(action.parentElement.getAttribute("href"));
 
-actionLink.forEach((action) => {
-  action.addEventListener("click", (e) => {
-    e.preventDefault();
-    navigator.clipboard.writeText(action.parentElement.getAttribute("href"));
-
-    document.getElementById("toast").innerHTML = `
+        document.getElementById("toast").innerHTML = `
         <div class="toast-container">
-            <p><strong> ${action.parentElement.innerText} </strong> </p>
+            <p><strong>${action.parentElement.innerText}</strong> kopyalandı.</p>
         </div>
     `;
 
+        setTimeout(() => {
+            document.querySelector("#toast .toast-container").classList.add("toast-gone");
+        }, 300);
 
-    setTimeout(() => {
-      document
-        .querySelector("#toast .toast-container")
-        .classList.add("toast-gone");
-    }, 300);
-
-    setTimeout(() => {
-      document.querySelector("#toast .toast-container").remove();
-    }, 2000);
-  });
+        setTimeout(() => {
+            document.querySelector("#toast .toast-container").remove();
+        }, 2000);
+    });
 });
 
+function requestAudioPermission() {
+    var audio = document.getElementById("myAudio");
+    audio.play();
+    var enterButton = document.querySelector(".button");
+    enterButton.disabled = true;
+    enterButton.style.opacity = "0.5"; 
+}
 
 
 document.querySelectorAll(".sosmed i").forEach((sosmed) => {
@@ -39,15 +42,10 @@ document.querySelectorAll(".sosmed i").forEach((sosmed) => {
   });
 });
 
-
-document.addEventListener("scroll", (e) => {
-  document.querySelector(".bg-text-animation").style.transform = `translateX(${
-    window.scrollY / 10
-  }px)`;
+document.addEventListener("scroll", () => {
+    document.querySelector(".bg-text-animation").style.transform = `translateX(${window.scrollY / 10}px)`;
 });
 
-const preloader = document.querySelector(".preloader");
-
 window.addEventListener("load", () => {
-  preloader.style.display = "none";
+    document.querySelector(".preloader").style.display = "none";
 });
